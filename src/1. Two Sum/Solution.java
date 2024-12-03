61d22af63e3f54b81bdb3ae70228a13204014f9a
Brian Britt <54298777+bmbritt@users.noreply.github.com>
2024-12-03 12:50:00 -0500

    Complete Two Sum Problem

--- src/1. Two Sum/Solution.java
+++ src/1. Two Sum/Solution.java
@@ -1,5 +1,32 @@
+import java.util.HashMap;
+
+/*
+ * This solution runs in linear O(N) time
+ * and makes use of a HashMap to avoid
+ * the more common O(N^2) solution that 
+ * evaluates every possible sum from the 
+ * given array
+ */
+
+
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+
+        // v : i such that value v (key) is found at i index (value) of nums
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int targetAddend = target - nums[i];
+
+            if (map.containsKey(targetAddend)) {
+                return new int[] {i, map.get(targetAddend)};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        //Given the problem parameters of exactly 1 solution, this should never happen.
+        return null;
     }
 }
