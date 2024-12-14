a49d6bb553b0059cc6c21748761c6f727a6fb27b
Brian Britt <54298777+bmbritt@users.noreply.github.com>
2024-12-13 22:35:05 -0500

    Complete 191. Number of 1 Bits.

--- src/191. Number of 1 Bits/Solution.java
+++ src/191. Number of 1 Bits/Solution.java
@@ -1,6 +1,10 @@
 class Solution {
     public int hammingWeight(int n) {
-        // bitwise operators (| , &, ~, ^)
-        return -1;
+        int result = 0;
+        while (n > 0) {
+            n &= (n-1);
+            result ++;
+        }
+        return result;
     }
 }
