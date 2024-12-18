fd5649004fa5162bfbb959e4cb530945a1ee2b32
Brian Britt <54298777+bmbritt@users.noreply.github.com>
2024-12-17 23:40:48 -0500

    Begin implementation of Climbing Stairs.

--- src/70. Climbing Stairs/Solution.java
+++ src/70. Climbing Stairs/Solution.java
@@ -1,6 +1,5 @@
 class Solution {
     public int climbStairs(int n) {
-        return -1;
-        // can use memoization to store answer for 1 step, use that to build to 2...n steps
+        return n <= 1 ? 1 : climbStairs(n - 1) + climbStairs(n - 2);
     }
 }
