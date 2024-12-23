779feda97291949306a9e902a017faad9318247b
Brian Britt <54298777+bmbritt@users.noreply.github.com>
2024-12-22 23:45:31 -0500

    Complete 270. Contains Duplicate.

--- src/270. Contains Duplicate/Solution.java
+++ src/270. Contains Duplicate/Solution.java
@@ -1,5 +1,16 @@
+import java.util.HashSet;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+
+
+        for (int number : nums) {
+            if (!set.add(number)) {
+                return true;
+            }
+        }
+
         return false;
     }
 }
