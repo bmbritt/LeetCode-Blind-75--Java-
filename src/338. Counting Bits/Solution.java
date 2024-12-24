a898fdfc7c4adcd89cb386acd64c0a82903304f8
Brian Britt <54298777+bmbritt@users.noreply.github.com>
2024-12-23 23:59:37 -0500

    Add placeholder.

--- src/338. Counting Bits/Solution.java
+++ src/338. Counting Bits/Solution.java
@@ -1,6 +1,8 @@
 class Solution {
     public int[] countBits(int n) {
-        return null;
+        
         // return hamming weight of each value in ranage [0, n+1] 
+        int[] result = null;
+        return result;
     }
 }
