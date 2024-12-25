93707c2a51bce271c1605e5adf9fe75650e0b807
Brian Britt <54298777+bmbritt@users.noreply.github.com>
2024-12-25 00:10:55 -0500

    Complete 338. Counting Bits.

--- src/338. Counting Bits/Solution.java
+++ src/338. Counting Bits/Solution.java
@@ -1,22 +1,19 @@
 class Solution {
     public int[] countBits(int n) {
         
-        // return hamming weight of each value in ranage [0, n+1], dynamic programming 
         int[] result = new int[n+1];
+        result[0] = 0;
+        int offset = 1;
 
+        for (int i = 1; i < result.length; i++) {
 
-        for (int i = 0; i < result.length; i++) {
-
-
-
-
-
-
-
-
-
+            if (offset * 2 == i) {
+                offset *= 2;
+            }
             
+            result[i] = 1 + result[i - offset];
         }
+
         return result;
     }
 }
