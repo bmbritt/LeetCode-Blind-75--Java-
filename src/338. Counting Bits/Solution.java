0fc378d36eaca9cfd58df37ee766b8926fbb8685
Brian Britt <54298777+bmbritt@users.noreply.github.com>
2024-12-24 23:55:16 -0500

    DP implementation of Counting Bits

--- src/338. Counting Bits/Solution.java
+++ src/338. Counting Bits/Solution.java
@@ -1,8 +1,22 @@
 class Solution {
     public int[] countBits(int n) {
         
-        // return hamming weight of each value in ranage [0, n+1] 
-        int[] result = null;
+        // return hamming weight of each value in ranage [0, n+1], dynamic programming 
+        int[] result = new int[n+1];
+
+
+        for (int i = 0; i < result.length; i++) {
+
+
+
+
+
+
+
+
+
+            
+        }
         return result;
     }
 }
