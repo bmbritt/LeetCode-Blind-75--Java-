a52b1751adb355ba31cd9c4e969bde4b1c75dc48
Brian Britt <54298777+bmbritt@users.noreply.github.com>
2024-12-31 23:54:22 -0500

    Add two pointer solution for 121. Stock price.

--- src/121. Best Time to Buy and Sell Stock/Solution.java
+++ src/121. Best Time to Buy and Sell Stock/Solution.java
@@ -1,5 +1,34 @@
+
+
+
 class Solution {
     public int maxProfit(int[] prices) {
-        return -1;
+
+        int profit = 0;
+
+
+        
+
+
+
+
+
+
+
+        return profit > 0 ? profit : 0;
+
+       
+
+
+
+       // sort the array, compare min and max, make sure max comes after the min chronologically
+
+
     }
+
+
+    
+
+
+ 
 }
