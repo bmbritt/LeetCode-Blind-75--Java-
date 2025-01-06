9be47a4d1f0d76d65a43997ddcca8353315d1744
Brian Britt <bmbritt@live.unc.edu>
2025-01-05 22:48:17 -0500

    Complete 21. Merge Two Sorted Lists.

--- src/21. Merge Two Sorted Lists/Solution.java
+++ src/21. Merge Two Sorted Lists/Solution.java
@@ -1,4 +1,3 @@
-
 //Definition for singly-linked list.
 class ListNode {
     int val;
@@ -15,35 +14,36 @@ class ListNode {
         this.val = val;
         this.next = next;
     }
+
 }
 
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null || list2 == null) {
+            return (list1 == null) ? list2 : list1;
+        } else {
+            ListNode BaseList = (list1.val <= list2.val) ? list1 : list2;
+            ListNode MergeList = (BaseList == list1) ? list2 : list1;
+            ListNode BaseElement = BaseList;
 
-        ListNode baseList = list1.val <= list2.val ? list1 : list2;
-        ListNode merginList = baseList == list1 ? list2 : list1;
-     
-        
-
-
-
-
-        
-
-
-
-        // find pivot, insert until original list's next element is less than next of new list, find pivot again
-
-        // find pivot
-        // while l2 val <= l1 next from splice location, insert elements
-        // find new pivot
-        // repeat
-        // end when l1 or l2 runs out of elements
-        //recursive solution
-
-        
-
-
-        return null;
+            while (MergeList != null) {
+                if (BaseElement.next == null) {
+                    BaseElement.next = MergeList;
+                    break;
+                }
+                if (MergeList.val > BaseElement.next.val) {
+                    BaseElement = BaseElement.next;
+                    continue;
+                } else {
+                    ListNode MergeListRemainder = MergeList.next;
+                    MergeList.next = BaseElement.next;
+                    BaseElement.next = MergeList;
+                    
+                    MergeList = MergeListRemainder;
+                    BaseElement = BaseElement.next;
+                }
+            }
+            return BaseList;
+        }
     }
 }
