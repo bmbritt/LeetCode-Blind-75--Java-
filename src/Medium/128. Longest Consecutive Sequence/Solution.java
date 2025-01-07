0d6fd097c1c6b4a9df2b3175ff42f6960b205279
Brian Britt <bmbritt@live.unc.edu>
2025-01-07 00:23:22 -0500

    Complete 128. Longest Consecutive Sequence.

--- src/Medium/128. Longest Consecutive Sequence/Solution.java
+++ src/Medium/128. Longest Consecutive Sequence/Solution.java
@@ -2,13 +2,26 @@ import java.util.HashSet;
 
 class Solution {
     public int longestConsecutive(int[] nums) {
+        int result = 0;
         HashSet<Integer> set = new HashSet<Integer>();
+
         for (int number : nums) {
             set.add(number);
         }
 
+        for (int number : nums) {
+            if (!set.contains(number-1)) {
+                int element = number;
+                int numConsecutive = 1;
 
-        
-        return -1;
+                while (set.contains(element + 1)) {
+                    set.remove(element);
+                    numConsecutive++;
+                    element++;
+                }
+                result = Math.max(result, numConsecutive);
+            }
+        }
+        return result;
     }
 }
