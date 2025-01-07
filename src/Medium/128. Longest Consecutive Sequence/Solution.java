ac1c3bfd45ce1eb5ad87651dfc71f5520c085739
Brian Britt <bmbritt@live.unc.edu>
2025-01-06 23:47:31 -0500

    Add set implementation of 128. Longest Consecutive Sequence

--- src/Medium/128. Longest Consecutive Sequence/Solution.java
+++ src/Medium/128. Longest Consecutive Sequence/Solution.java
@@ -1,5 +1,14 @@
+import java.util.HashSet;
+
 class Solution {
     public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int number : nums) {
+            set.add(number);
+        }
+
+
+        
         return -1;
     }
 }
