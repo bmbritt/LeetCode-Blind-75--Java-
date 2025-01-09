b3f6091ea0885a5e657235aeb16131cd54f6d27f
Brian Britt <bmbritt@live.unc.edu>
2025-01-08 23:51:48 -0500

    Add set to 15. 3Sum.

--- src/Medium/128. Longest Consecutive Sequence/15. 3Sum/Solution.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        return null;
-    }
-}
---
+++ src/Medium/15. 3Sum/Solution.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        HashSet<Integer> numSet = new HashSet<Integer>();
+
+        for (int num : nums) {
+            numSet.add(num);
+        }
+        
+      
+
+
+
+
+        
+        return null;
+    }
+}
