8a69b6af2e6474bd5fa65a388d443bab1b1510ee
Brian Britt <54298777+bmbritt@users.noreply.github.com>
2025-03-21 11:08:14 -0400

    Add problem definition.

--- src/Medium/11. Container With Most Water/Solution.java
+++ src/Medium/11. Container With Most Water/Solution.java
@@ -1,5 +1,8 @@
 class Solution {
     public int maxArea(int[] height) {
+
+        // two lines (indices) such that a container formed between them and with the x axis is maximized for area
+
         return -1;
     }
 }
